84dba89630a129258e19a43ee92b024301cdac87
Harold <75729237+surelle-ha@users.noreply.github.com>
2026-01-17 09:28:18 +0800

    fix(na): modify index - test

---
+++ api/vercel.js
@@ -0,0 +1,7 @@
+import app from '@/app.js';
+
+export default {
+  fetch(request) {
+    return app.fetch(request);
+  },
+};
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import app from '@/app.js';
+import { serve } from 'bun';
 
-export default {
-  fetch(request) {
-    return app.fetch(request);
-  },
-};
+const port = process.env.PORT || 3030;
+const bunApp = serve({
+  port,
+  fetch: app.fetch,
+  idleTimeout: 20,
+});
+
+console.log(`server is running visit ${bunApp.url}doc for docs`);
