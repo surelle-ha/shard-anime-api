075380fa324d5f43d6ccc5f9ad941433d413282c
Harold <75729237+surelle-ha@users.noreply.github.com>
2026-01-17 09:22:26 +0800

    fix(na): modify index - test

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import app from '@/app.js';
-import { serve } from 'bun';
 
-const port = process.env.PORT || 3030;
-const bunApp = serve({
-  port,
-  fetch: app.fetch,
-  idleTimeout: 20,
-});
-
-console.log(`server is running visit ${bunApp.url}doc for docs`);
+export default {
+  fetch(request) {
+    return app.fetch(request);
+  },
+};
